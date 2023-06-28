569bde580a30b5517abc165d2f2169135c94e1dc
OfentseLearnsCode <malebyeoc@gmail.com>
2023-06-28 08:20:21 +0200

    Initial Commit

---
+++ Variable_playground/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Variable_playground
+//
+//  Created by DA MAC M1 137 on 2023/06/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
